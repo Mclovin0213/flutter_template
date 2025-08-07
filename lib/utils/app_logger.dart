44840a9cf84fd495970680f241b004b6982c1fb9
Mclovin0213 <javellaneda0213@gmail.com>
2025-08-07 15:53:33 -0700

    start over

---
+++ lib/utils/app_logger.dart
@@ -0,0 +1,48 @@
+// -----------------------------------------------------------------------
+// Filename: app_logger.dart
+// Description: This file contains a simple logger class that wraps
+//              the Flutter print method. This allows for easy logging
+//              to the console.
+
+//////////////////////////////////////////////////////////////////////////
+// Imports
+//////////////////////////////////////////////////////////////////////////
+// Flutter external package imports
+import 'package:logger/logger.dart';
+
+//////////////////////////////////////////////////////////////////////////
+// Class Definition
+//////////////////////////////////////////////////////////////////////////
+class AppLogger {
+  static void print(String message) {
+    var logger = Logger(
+      filter: null,
+      printer: PrettyPrinter(colors: true, errorMethodCount: 2),
+    );
+    logger.i(message);
+  }
+
+  static void error(String error) {
+    var logger = Logger(
+      filter: null,
+      printer: PrettyPrinter(colors: true, errorMethodCount: 2),
+    );
+    logger.e(error, error: '');
+  }
+
+  static void warning(String warning) {
+    var logger = Logger(
+      filter: null,
+      printer: PrettyPrinter(colors: true, errorMethodCount: 2),
+    );
+    logger.w(warning);
+  }
+
+  static void debug(String debug) {
+    var logger = Logger(
+      filter: null,
+      printer: PrettyPrinter(colors: true, errorMethodCount: 0, methodCount: 0),
+    );
+    logger.d(debug);
+  }
+}
